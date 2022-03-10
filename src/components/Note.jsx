d3cc759f496dc2bbcd80add0ba82fcc211479ac3
Francis Adu Gyamfi <88909721+Adu-Gyamfi-Francis@users.noreply.github.com>
2022-03-10 13:34:40 +0000

    Initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Heading of the Note</h1>
+      <p>This is the paragraph for the Note.</p>
+    </div>
+  );
+}
+
+export default Note;
